04c3906825a60f8a8e1dd703ce70e61dbdf591c8
Madison Pfaff <mpfaff@live.unc.edu>
2015-09-16 19:46:26 -0400

    Create Position.java
    Structure for the position of the mouse

---
+++ Position.java
@@ -0,0 +1,48 @@
+package assignment1;
+
+public class Position {
+	private int x;
+	private int y;
+	private Position parent;
+	//Total distance from start so far
+	private int totalCost;
+	
+	public Position(int x, int y, Position parent){
+		this.x = x;
+		this.y = y;
+		this.parent = parent;
+		
+		if(parent != null)
+			this.totalCost = parent.getTotalCost() + 1;
+	}
+	
+	public int x(){
+		return x;
+	}
+	
+	public int y(){
+		return y;
+	}
+	
+	public int getTotalCost(){
+		return totalCost;
+	}
+
+	public Position parent(){
+		return parent;
+	}
+	
+	//Manhattan Distance
+	public int getDistanceFrom(Position p){
+		return Math.abs(this.x() - p.x()) + Math.abs(this.y() - p.y());
+	}
+	
+	public String toString(){
+		return "(" + x + ", " + y + ")";
+	}
+	
+	public boolean equals(Position p){
+		return (this.x() == p.x() && this.y() == p.y());
+	}
+
+}
